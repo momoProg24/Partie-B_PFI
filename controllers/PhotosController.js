06bb19e11470ff1b12874198d2d1b1229e872d00
Andrew Kartsakis <andrew.kartsakis@outlook.com>
2023-12-13 18:27:51 -0500

    f g f cx
    crercekmnknj

--- controllers/PhotosController.js
+++ controllers/PhotosController.js
@@ -3,11 +3,18 @@ import Repository from '../models/repository.js';
 import PhotoModel from '../models/photo.js';
 //import PhotoLikeModel from '../models/photoLike.js';
 import Controller from './Controller.js';
+import * as utilities from "../utilities.js";
 
 export default
     class Photos extends Controller {
     constructor(HttpContext) {
         super(HttpContext, new Repository(new PhotoModel()), Authorizations.user());
-       // this.photoLikesRepository = new Repository(new PhotoLikeModel());
+        // this.photoLikesRepository = new Repository(new PhotoLikeModel());
     }
+    /*
+        post(data) {
+            data.Date = utilities.nowInSeconds();
+            this.HttpContext.response.JSON(data)
+        }
+    */
 }
